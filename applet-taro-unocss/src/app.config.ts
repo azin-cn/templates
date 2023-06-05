4b1dd60e6a19234636eeb7d87bc2e527a0e26988
azin-cn <Mr.MF@foxmail.com>
2023-06-05 22:47:19 +0800

    chore: add applet-taro-unocss template

---
+++ applet-taro-unocss/src/app.config.ts
@@ -0,0 +1,9 @@
+export default defineAppConfig({
+  pages: ["views/index/index"],
+  window: {
+    backgroundTextStyle: "light",
+    navigationBarBackgroundColor: "#fff",
+    navigationBarTitleText: "WeChat",
+    navigationBarTextStyle: "black",
+  },
+});
